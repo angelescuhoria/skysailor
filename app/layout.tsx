13a185ade1975da81fea49dda2b760d65cebc9e4
Horia Angelescu <angelescuhoria@gmail.com>
2025-02-12 14:37:46 +0200

    [frontend/fix] fixed footer position

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,16 +21,16 @@ export default function Layout({
       suppressHydrationWarning
       className={`${inter.variable} ${roboto.variable}`}
     >
-      <body className="w-screen relative flex flex-col gap-14 px-4 md:px-8 lg:px-16 2xl:px-32 lg:gap-28">
+      <body className="w-screen relative min-h-screen flex flex-col gap-14 px-4 md:px-8 lg:px-16 2xl:px-32 lg:gap-28">
         <div className="background-image bg-[url('/images/default.jpg')]"></div>
         <div className="overlay"></div>
         <Header />
         <QueryProvider>
-          <main>
+          <main className="mb-24 lg:mb-0">
             {children} <ReactQueryDevtools initialIsOpen={false} />
           </main>
         </QueryProvider>
-        <footer className="">
+        <footer className="absolute bottom-0 left-0 w-full px-4 md:px-8 lg:px-16 2xl:px-32">
           <Footer />
         </footer>
       </body>
